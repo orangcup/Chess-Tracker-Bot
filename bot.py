29c7e8f970d749155e016edc3c27938c216aa0e0
orangcup <iansun768@gmail.com>
2025-07-01 21:35:45 -0400

    starting code

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+
+bot.run(str(TOKEN))
