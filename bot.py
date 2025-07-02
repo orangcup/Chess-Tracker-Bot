29d7c328968495c7596dd703aeff6608cdb59820
orangcup <iansun768@gmail.com>
2025-07-02 10:53:32 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,16 +3,20 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+import requests
+import chess.pgn
+import io
+
 load_dotenv()
 
-intents = discord.Intents.default() # Create a default intents object
+intents = discord.Intents.default() # make a default intents object
 intents.message_content = True # Enable message content intent
 
-bot = discord.Client(intents=intents) # Create a client instance with the specified intents
+bot = discord.Client(intents=intents) # make the bot client
 
 @bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
+async def on_ready(): #when bot is loaded
+    print(f'Logged in as {bot.user}') #it should say so in console
 
 @bot.event
 async def on_message(message):
@@ -20,6 +24,10 @@ async def on_message(message):
         return
     if message.content.startswith('!hello'):
         await message.channel.send('Hello! I am your Chess Tracker Bot!')
+    if bot.user in message.mentions: # if the bot is mentioned...
+        pass #will update
+    if message.content.startswith("!update_username"):
+        
 
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
